7a6665c0f1e41b6e752a103ae81fb59388fafd13
lzs <lzs@justep.com>
2018-05-30 17:58:25 +0800

    update

--- console-web/src/main/java/org/console/web/controller/HelloController.java
+++ console-web/src/main/java/org/console/web/controller/HelloController.java
@@ -1,14 +1,20 @@
 package org.console.web.controller;
 
+import org.console.web.property.RedisConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Autowired
+	private RedisConfig config;
+	
 	@GetMapping("/test1")
 	public Object test() {
-		return "";
+		
+		return config;
 	}
 	
 }
