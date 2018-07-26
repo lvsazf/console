93acaf47e852f3c38c1fcbcdec779d062f384285
lzs <lzs@justep.com>
2018-07-26 14:48:59 +0800

    fastjson

--- console-web/src/main/java/org/console/web/config/AppConfig.java
+++ console-web/src/main/java/org/console/web/config/AppConfig.java
@@ -1,10 +1,31 @@
 package org.console.web.config;
 
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.justep.console.core.service.HttpClient;
 
 @Configuration
 public class AppConfig {
-	
-	
-	
+
+	//引入Fastjson解析json，不使用默认的jackson
+	//必须在pom.xml引入fastjson的jar包，并且版必须大于1.2.10
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		//1、定义一个convert转换消息的对象
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		//2、添加fastjson的配置信息
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		//3、在convert中添加配置信息
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+		//4、将convert添加到converters中
+		HttpMessageConverter<?> converter = fastConverter;
+		return new HttpMessageConverters(converter);
+	}
 }
